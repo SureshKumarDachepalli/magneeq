b401594ee23bcef84439bacae8d2aa9f45a5997f
SureshKumarDachepalli <144228613+SureshKumarDachepalli@users.noreply.github.com>
2024-02-05 15:57:01 +0530

    Update gmagniq.java

--- src/gm/gmagniq.java
+++ src/gm/gmagniq.java
@@ -3,7 +3,7 @@ package gm;
 public class gmagniq {
 
 	public static void main(String[] args) {
-		System.out.println(" push the codes");
+		System.out.println(" pull the codes");
      int a=10;
      int b=10;
      int c=a+b;
