3e1abdfe4e312ddb3b70f6cc8037aaaf50d9a0f7
dell <dell@DESKTOP-I89L11M>
2024-02-05 15:48:03 +0530

    i am giving another push

--- src/gm/gmagniq.java
+++ src/gm/gmagniq.java
@@ -3,9 +3,9 @@ package gm;
 public class gmagniq {
 
 	public static void main(String[] args) {
-		System.out.println("im pull the code");
-     int a=20;
-     int b=30;
+		System.out.println(" push the codes");
+     int a=10;
+     int b=10;
      int c=a+b;
      System.out.println(c);
 	}
